38bbc99c25d54bc35ba2b8ff20734936d95a1321
claytron <claytron@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-06-24 07:57:35 +0000

    bumping version for next release, adding changes stub for next release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.7dev'
+version = '0.8dev'
 
 long_description = (
     read('README.txt')
