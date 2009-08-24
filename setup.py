a1c2d6c3870c6ea3ae380f0f719a5ac8f542912b
claytron <claytron@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-24 15:16:32 +0000

    modifying version for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0dev'
+version = '1.0'
 
 long_description = (
     read('README.txt')
