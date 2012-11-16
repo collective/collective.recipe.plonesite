1088d73fd8f552701a6787804b89df2e440250ad
Nolan Brubaker <nolan@sixfeetup.com>
2012-11-16 17:28:56 -0500

    Update changelog.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.8.0'
+version = '1.8.1'
 
 long_description = (
     read('README.txt')
